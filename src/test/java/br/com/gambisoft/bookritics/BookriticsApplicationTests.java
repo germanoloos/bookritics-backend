3c6e1d75acb53a308352fca91786cb11ac3bf793
germanoloos <germanoloos@hotmail.com>
2019-04-12 16:01:43 -0300

    Rename pom.xml

--- src/test/java/br/com/gambisoft/bookritics/BookriticsApplicationTests.java
+++ src/test/java/br/com/gambisoft/bookritics/BookriticsApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.elumini.bookboot;
+package br.com.gambisoft.bookritics;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BookBootRestApplicationTests {
+public class BookriticsApplicationTests {
 
 	@Test
 	public void contextLoads() {
